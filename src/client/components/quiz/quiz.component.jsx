92f138e95365b90bf6c063de0fc55c06697ba370
Eric K <kreitere@gmail.com>
2019-12-17 21:55:26 -0500

    Update: pulling data from API and randomizing question data is complete

---
+++ src/client/components/quiz/quiz.component.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+export default class Quiz extends Component {
+  constructor() {
+    super();
+    this.state = {
+      questionList: [],
+      activeQuestion: null,
+      count: 0,
+      questionResults: { correct: 0, incorrect: 0, total: 0 },
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:4000/api/questions')
+      .then(res => res.json())
+      .then(data => {
+        let questions = data.results;
+        this.randomizeArr(questions);
+        this.setState({ questionList: questions });
+      })
+      .then(() => this.setActiveQuestion());
+  }
+
+  setActiveQuestion() {
+    let { count, questionList, activeQuestion } = this.state;
+    if (count < 5 || null) {
+      this.setState({
+        count: count++,
+        activeQuestion: questionList.pop(),
+        questionList: questionList,
+      });
+    }
+  }
+
+  //Fisher-Yates randomization to randomize the Arr in o(n) time
+  randomizeArr(inputArr) {
+    for (let i = inputArr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      //ES6 syntax to swap 2 variables at the same time
+      [inputArr[i], inputArr[j]] = [inputArr[j], inputArr[i]];
+    }
+  }
+
+  render() {
+    let { activeQuestion } = this.state;
+    return <div className="quiz-container"></div>;
+  }
+}
